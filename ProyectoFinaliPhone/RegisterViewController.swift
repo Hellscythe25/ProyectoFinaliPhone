69dfd547263c735a280006798b76069e0475730d
Hellscythe25 <46532421+Hellscythe25@users.noreply.github.com>
2022-05-22 05:26:21 -0500

    bug on RegisterViewController

--- ProyectoFinaliPhone/RegisterViewController.swift
+++ ProyectoFinaliPhone/RegisterViewController.swift
@@ -10,54 +10,42 @@ import UIKit
 class RegisterViewController: UIViewController{
     
     @IBOutlet private weak var anchorBottomScroll: NSLayoutConstraint!
-}
-//MARK - Life Cycle
-extension RegisterViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction private func tapToCloseKeyboard(_ sender: UITapGestureRecognizer){
+        self.view.endEditing(true)
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.registerKeyboardNotification()
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.unregisteredKeyboardNotification()
+        self.unregisterKeyboardNotification()
     }
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-    }
+    
+    
+    
+    
 }
-//MARK - Action Events
-extension RegisterViewController{
-    @IBAction private func tapToCloseKeyboard(_ sender: UITapGestureRecognizer){
-        self.view.endEditing(true)
-    }
-}
-
-//MARK - Keyboard Events
+//MARK: Keyboard Events
 extension RegisterViewController{
     private func registerKeyboardNotification(){
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    private func unregisteredKeyboardNotification(){
+    private func unregisterKeyboardNotification(){
         NotificationCenter.default.removeObserver(self)
     }
-    
     @objc private func keyboardWillShow(_ notification: Notification){
         let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
-        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect ?? .zero
         UIView.animate(withDuration: animationDuration){
             self.anchorBottomScroll.constant = keyboardFrame.height
             self.view.layoutIfNeeded()
         }
     }
     @objc private func keyboardWillHide(_ notification: Notification){
-        
         let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
         UIView.animate(withDuration: animationDuration){
             self.anchorBottomScroll.constant = 0
