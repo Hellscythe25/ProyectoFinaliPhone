d86da5bbe99a7dc12c389160115c284a64367849
Hellscythe25 <46532421+Hellscythe25@users.noreply.github.com>
2022-07-23 02:47:04 -0500

    Register working // missing firebase app check for data storing

--- ProyectoFinaliPhone/NewRegisterViewController.swift
+++ ProyectoFinaliPhone/NewRegisterViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
+
 class NewRegisterViewController: UIViewController{
     
     @IBOutlet private weak var anchorBottomScroll: NSLayoutConstraint!
@@ -31,9 +33,74 @@ class NewRegisterViewController: UIViewController{
             self.unregisterKeyboardNotification()
         }
     
+    func isPasswordValid(_ password : String) -> Bool{
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    func validateFields() -> String? {
+        
+        if passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || password2TextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || nameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ==  "" || lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Datos sin llenar"
+        }
+        
+        if passwordTextField.text! != password2TextField.text! {
+            return "Contraseñas distintas"
+        }
+        
+        let cleannedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if isPasswordValid(cleannedPassword) == false {
+            return "Contraseña poco segura"
+        }
+        
+        return nil
+    }
+    
     @IBAction func signUpButtonAction(_ sender: Any){
         
+        let error = validateFields()
         
+        if error != nil{
+            showError(error!)
+            
+        }
+        else{
+            
+            let name = nameTextField.text!
+            let lastName = lastNameTextField.text!
+            let email = emailTextField.text!
+            let password = passwordTextField.text!
+            
+            Auth.auth().createUser(withEmail: email, password: password){
+                (result, err) in
+                if err != nil {
+                    self.showError("Error creando usuario")
+                }
+                else {
+                    let db = Firestore.firestore()
+                    
+                    db.collection("users").addDocument( data: ["email":email,"name": name, "lastName":lastName, "uid":result!.user.uid]) { (error) in
+                        if error != nil {
+                            self.showError("Data not saved correctly")
+                        }
+                    }
+                    let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                    
+                    self.view.window?.rootViewController = homeViewController
+                    self.view.window?.makeKeyAndVisible()
+                }
+            }
+            
+        }
+        
+    }
+    
+    func showError(_ message:String){
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+        self.present(alertController, animated: true, completion: nil)
     }
     
 }
